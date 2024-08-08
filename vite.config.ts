cbcd239e970980c3cccf7e99b3e404075778a221
lanarmah <lanarmah@gmail.com>
2024-08-08 20:59:20 +0000

    chore: support vite meta env load

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import ImportMetaEnvPlugin from '@import-meta-env/unplugin'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		ImportMetaEnvPlugin.vite({
+			example: '.env.example',
+		}),
 		react(),
 		VitePWA({
 			base: '/',
