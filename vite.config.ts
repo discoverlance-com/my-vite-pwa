9fce38943bf39528ab1930ed1aba140d8fe77af7
lanarmah <lanarmah@gmail.com>
2024-08-08 21:20:23 +0000

    fix: remove imports for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import ImportMetaEnvPlugin from '@import-meta-env/unplugin'
+// import ImportMetaEnvPlugin from '@import-meta-env/unplugin'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		ImportMetaEnvPlugin.vite({
-			example: '.env.example',
-		}),
+		// ImportMetaEnvPlugin.vite({
+		// 	example: '.env.example',
+		// }),
 		react(),
 		VitePWA({
 			base: '/',
